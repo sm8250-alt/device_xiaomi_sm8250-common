8ee31c93a980dd77df5e658c1c36fc502fccbc41
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-29 16:43:55 +0100

    sm8250-common: Link Dolby Atmos dependencies against v33 libstagefright_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,9 @@ function blob_fixup() {
         vendor/etc/media_codecs_kona.xml)
             sed -i "/media_codecs_dolby_audio.xml/d" "${2}"
             ;;
+        vendor/lib64/libdlbdsservice.so | vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
